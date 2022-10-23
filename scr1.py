7cf37aed945b3944ebd17ca082a8c310edd8a2a7
Calm Duane <deadbattery97@gmail.com>
2022-10-23 11:11:00 -0400

    changed cookbook to dictionary

--- scr1.py
+++ scr1.py
@@ -1,35 +1,48 @@
-
-
 # Let's make a calculator!
+# Create recipes to populate the cookbook
 
-# Let's make a 'recipe' class
+cookbook = {}
 
 class recipe:
-    def __init__(self, input, output):
-        self.input = input
-        self.output = output
+    def __init__(self, inway, outway, stoich, time, name):
+        self.inway = inway
+        self.output = outway
+        self.stoich = stoich
+        self.time = time
+        self.name = name
 
-cop_in = {
-    'copper': 1,
-    'time': 0.5
-}
 
-cop_out = {
-    'copper_cable': 2
-}
+inway = ['copper']
+outway = ['copper_cable']
+stoich = {'copper':1, 'copper_cable':2}
 
-copper_wire = recipe(cop_in, cop_out)
-print(copper_wire.input)
+copper_wire = recipe(inway, outway, stoich, 0.5, 'copper_wire')
+cookbook.update({'copper_wire':copper_wire})
 
-circ_in = {
-    'time': 0.5,
-    'copper_cable': 3,
-    'iron_plate': 1
-}
+inway = ['copper_cable', 'iron_plate']
+outway = ['electronic_circuit']
+stoich = {'copper_cable':3, 'iron_plate':1, 'electronic_circuit':1}
 
-circ_out = {
-    'electronic circuit': 1
-}
+electronic_circuit = recipe(inway, outway, stoich, 0.5,'electronic_circuit')
+cookbook.update({'electronic_circuit':electronic_circuit})
 
-electronic_circuit = recipe(circ_in, circ_out)
+# End cookbook
+
+# Begin main program logic
+
+bottleneck = ('electronic_circuit', 1)
+
+x = cookbook.get(bottleneck[0])
+print(x.inway)
+# for i in x.inway:
+#     print(i)
+#     if i in cookbook.keys():
+#         print(f'{i}in cookbook')
+#     else:
+#         print(f'{i} not in cookbook')
+
+# if bottleneck[0] in cookbook:
+#     print(cookbook('electronic_circuit').get(inway))
+# else:
+#     print('False')
 
